442a62ad18a1bb1f5689d326d60c5fb913a63fb2
Ian Johnson <ianjjohnson@icloud.com>
2016-11-14 15:56:54 -0600

    In class 22

--- Class_22/umbrella_projects.ex
+++ Class_22/umbrella_projects.ex
@@ -6,3 +6,14 @@
       # b) umbrella projects
         # - each application is a subproject
         # - managed as a whole or independently
+
+#Umbrella projects unify management, maintenance, and testing
+#Feel more like one big app
+
+#Umbrella project:
+  #Looks like a regular project
+  #Has no code
+  #Has an apps/ directory
+    #Inside the apps/ directory, all the apps live
+    #Dependencies and build products are all stored at the top level
+    
