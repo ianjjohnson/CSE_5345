5031ec954d81b4cb9167e5dc84d4b3e58e02ee35
Ian Johnson <ianjjohnson@icloud.com>
2016-11-16 16:27:02 -0600

    Add Class23

---
+++ Class_23/GenStage.ex
@@ -0,0 +1,10 @@
+#GenStage
+
+#Process Based
+#Pass events between themselves
+#Producers, consumers, and both
+#Support back-pressure
+
+#Back-pressure: saying "I can't handle this. Slow down the input"
+
+#GenStage handles flow control all the way down the pipeline
