8dc906c5f982658582c9c66f58f3c913abf7bd2b
Ian Johnson <ianjjohnson@icloud.com>
2016-11-30 16:33:00 -0600

    Day 25

---
+++ Class_25/Deployment.ex
@@ -0,0 +1,12 @@
+# Need to:
+  # gather app, dependencies, assets, runtime, etc. into single bundle
+  # copy bundle to server
+  # install, then start or upgrade
+
+#Distillery:
+  # Gather everything needed to run
+    # app, dependencies, assets, runtime
+  # Package into single bundle
+
+#Add Distillery to the deps list for an app
+#Setup release tree (mix release.init)
